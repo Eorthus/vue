17dbbf0ff1570ca41f46c523be23b0268e1dad3c
eorthus <oakenshielddis@gmail.com>
2022-02-25 22:32:01 +0300

    calc

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="display">
+      <input v-model.number="operand1" type="number" />
+      <input v-model.number="operand2" type="number" />
+      ={{ result }}
+    </div>
+    <div class="buttons">
+      <button v-on:click="result = operand1 + operand2">+</button>
+      <button v-on:click="result = operand1 - operand2">-</button>
+      <button v-on:click="result = operand1 * operand2">*</button>
+      <button v-on:click="result = del(operand1, operand2)">/</button>
+      <button v-on:click="result = Math.pow(operand1, operand2)">^</button>
+      <button v-on:click="result = fullpart(operand1, operand2)">%</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      operand1: 0,
+      operand2: 0,
+      result: 0,
+    };
+  },
+  methods: {
+    del(op1, op2) {
+      return (this.result = op1 / op2);
+    },
+
+    fullpart(op1, op2) {
+      return (this.result = op1 % op2);
+    },
+  },
+};
 </script>
 
 <style>
